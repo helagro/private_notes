38ebd53d271d80e7ac1a77097f56b4a812df3ddd
I <henrikl@tuta.io>
2022-06-24 21:29:27 +0200

    working dropbox auth-flow

--- lib/logic/dropbox_handler.dart
+++ lib/logic/dropbox_handler.dart
@@ -2,6 +2,9 @@ import 'package:url_launcher/url_launcher.dart';
 
 class DropboxHandler {
   void authorize() {
-    launchUrl(Uri(path: "https://flutter.dev"), webOnlyWindowName: "_blank");
+    launchUrl(
+        Uri.parse(
+            "https://www.dropbox.com/oauth2/authorize?client_id=y4hmk1pjerg1vvp&response_type=code"),
+        webOnlyWindowName: "_blank");
   }
 }
