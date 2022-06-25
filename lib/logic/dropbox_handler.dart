f13a5df8edf66f9a8c67919326d8ff58ddac0ac3
I <henrikl@tuta.io>
2022-06-25 10:08:28 +0200

    working auth flow

--- lib/logic/dropbox_handler.dart
+++ lib/logic/dropbox_handler.dart
@@ -1,10 +1,42 @@
 import 'package:url_launcher/url_launcher.dart';
+import 'package:http/http.dart' as http;
 
 class DropboxHandler {
+  static DropboxHandler? _instance;
+  static const _appKey = "y4hmk1pjerg1vvp";
+  String? _token;
+
+  static DropboxHandler getInstance() {
+    _instance ??= DropboxHandler();
+    return _instance!;
+  }
+
+  DropboxHandler() {}
+
+  void generateChallengePair() {}
+
   void authorize() {
     launchUrl(
         Uri.parse(
-            "https://www.dropbox.com/oauth2/authorize?client_id=y4hmk1pjerg1vvp&response_type=code"),
+            "https://www.dropbox.com/oauth2/authorize?client_id=$_appKey&response_type=code&code_challenge_method=plain&code_challenge=${getCodeChallengeStr()}"),
         webOnlyWindowName: "_blank");
+  }
+
+  String getCodeChallengeStr() {
+    return "FjbnHORmpnTF08DV45Xab6aUDCahywkSfx6kAe17pKoZXsJ3KP";
+  }
+
+  void token(String str) async {
+    Map<String, String> headers = {
+      "code": str,
+      "grant_type": "authorization_code",
+      "client_id": _appKey,
+      "code_verifier": getCodeChallengeStr()
+    };
+
+    print("dwa $headers");
+    http.Response res = await http
+        .post(Uri.parse("https://api.dropbox.com/oauth2/token"), body: headers);
+    print("people ${res.body}");
   }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -4,12 +4,23 @@ import 'package:private_notes/logic/dropbox_handler.dart';
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: TextButton(
-        child: Text("do something"),
-        onPressed: () {
-          DropboxHandler().authorize();
-        },
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            TextButton(
+              child: Text("do something"),
+              onPressed: () {
+                DropboxHandler().authorize();
+              },
+            ),
+            TextField(
+              onChanged: (value) {
+                DropboxHandler().token(value);
+              },
+            )
+          ],
+        ),
       ),
     );
   }
