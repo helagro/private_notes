b79d1290f08b2f08eb8930c17c2f6729f9995964
I <henrikl@tuta.io>
2022-06-25 10:08:07 +0200

    working auth flow

---
+++ test/dropbox_handler_test.dart
@@ -0,0 +1,11 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:private_notes/logic/dropbox_handler.dart';
+
+void main() {
+  print("hello world");
+  //DropboxHandler dropboxHandler = DropboxHandler();
+  //dropboxHandler.authorize();
+  //String? authCode = stdin.readLineSync(encoding: utf8);
+  //print("auth code: $authCode");
+}
