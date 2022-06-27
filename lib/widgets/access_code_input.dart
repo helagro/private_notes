0c7c26447fee078e9df51de65ca75ca6e7757c5c
I <henrikl@tuta.io>
2022-06-27 18:27:25 +0200

    better authorization code input field

--- lib/widgets/access_code_input.dart
+++ lib/widgets/access_code_input.dart
@@ -1,11 +1,18 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:opnot/widgets/icon_button_main.dart';
 
 import '../logic/dropbox_handler.dart';
 
-class AccessCodeInput extends StatelessWidget {
-  DropboxHandler _dropboxHandler = DropboxHandler.getInstance();
-  TextEditingController _controller = TextEditingController();
+class AccessCodeInput extends StatefulWidget {
+  @override
+  State<AccessCodeInput> createState() => _AccessCodeInputState();
+}
+
+class _AccessCodeInputState extends State<AccessCodeInput> {
+  final DropboxHandler _dropboxHandler = DropboxHandler.getInstance();
+  final TextEditingController _controller = TextEditingController();
+  bool _authCodeFormatIsValid = false;
 
   @override
   Widget build(BuildContext context) {
@@ -14,23 +21,36 @@ class AccessCodeInput extends StatelessWidget {
       child: Row(
         children: [
           Expanded(
-            child: TextField(
+            child: CupertinoTextField(
               controller: _controller,
-              decoration:
-                  const InputDecoration(labelText: "Enter access code here"),
+              placeholder: "Enter access code here",
+              onChanged: onChanged,
             ),
           ),
-          IconButtonMain(
-              iconData: Icons.send, onPressed: () => doStuff(context))
+          _authCodeFormatIsValid
+              ? IconButtonMain(
+                  iconData: CupertinoIcons.arrow_right_circle,
+                  onPressed: () => submitCode(context))
+              : Container()
         ],
       ),
     );
   }
 
-  void doStuff(BuildContext context) async {
-    await _dropboxHandler.generateToken(_controller.text);
-    print("will upload");
+  void onChanged(String value) {
+    final bool newAuthCodeFormatIsValid = value.isNotEmpty;
 
+    if (newAuthCodeFormatIsValid != _authCodeFormatIsValid) {
+      setState(() {
+        _authCodeFormatIsValid = newAuthCodeFormatIsValid;
+      });
+    }
+  }
+
+  void submitCode(BuildContext context) async {
+    await _dropboxHandler.generateToken(_controller.text);
+
+    if (!mounted) return;
     Navigator.pushNamed(context, "/editor");
     //await dropboxHandler.upload(Note("titel", "coontent"));
   }
